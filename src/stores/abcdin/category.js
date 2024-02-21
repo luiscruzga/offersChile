98528c919741fdf515be171c21180d4133f365c9
Luis Cruz <lcruzgar@emeal.nttdata.com>
2024-02-21 14:49:51 -0300

    feat: changes abcdin

--- src/stores/abcdin/category.js
+++ src/stores/abcdin/category.js
@@ -1,5 +1,5 @@
-const storeKey = 'abcdin';
-const { STORES } = require('../../config/config.json');
+const storeKey = "abcdin";
+const { STORES } = require("../../config/config.json");
 const { getDataUrl, saveFile } = require("../../utils");
 
 /**
@@ -8,31 +8,42 @@ const { getDataUrl, saveFile } = require("../../utils");
  */
 const getUrlCategories = (categories) => {
   const finalCategories = [];
-  categories.forEach(el => {
+  categories.forEach((el) => {
     finalCategories.push({
-      name: el.querySelector('span').textContent,
-      url: el.href
+      name: el.querySelector("span").textContent,
+      url: el.href.includes(STORES[storeKey].baseUrl)
+        ? el.href
+        : `${STORES[storeKey].baseUrl}${el.href}`,
     });
   });
 
   return finalCategories;
-}
+};
 
 /**
-* Permite obtener un listado de categorias desde la misma store
-*/
+ * Permite obtener un listado de categorias desde la misma store
+ */
 const getCategories = async () => {
   log.info(`Getting categories of [${STORES[storeKey].name}]`);
   const dom = await getDataUrl(STORES[storeKey].baseUrl);
-  
-  let categoriesInfo = getUrlCategories([...dom.window.document.querySelectorAll('.level0 > a')]);
+
+  let categoriesInfo = getUrlCategories([
+    ...dom.window.document.querySelectorAll(
+      '.desktop-menu > ul.nav > li.nav-item[role="menuitem"] > a'
+    ),
+  ]);
   if (STORES[storeKey].allowedCategories.length > 0) {
-    categoriesInfo = categoriesInfo.filter(category => STORES[storeKey].allowedCategories.filter(el => category.name.toLowerCase().includes(el.toLowerCase())).length > 0);
+    categoriesInfo = categoriesInfo.filter(
+      (category) =>
+        STORES[storeKey].allowedCategories.filter((el) =>
+          category.name.toLowerCase().includes(el.toLowerCase())
+        ).length > 0
+    );
   }
   saveFile(`${__dirname}/categories.json`, categoriesInfo);
   return categoriesInfo;
-}
+};
 
 module.exports = {
-  getCategories
-}
+  getCategories,
+};
--- src/stores/abcdin/products.js
+++ src/stores/abcdin/products.js
@@ -1,7 +1,7 @@
-const storeKey = 'abcdin';
-const { STORES } = require('../../config/config.json');
+const storeKey = "abcdin";
+const { STORES } = require("../../config/config.json");
 const STORE_NAME = STORES[storeKey].name;
-const { getDataUrl, replaceAll } = require('../../utils/');
+const { getDataUrl, replaceAll } = require("../../utils/");
 let lastVersion = 1;
 const totalPerPage = STORES[storeKey].totalProductsPerPage;
 
@@ -16,58 +16,117 @@ const totalPerPage = STORES[storeKey].totalProductsPerPage;
  */
 const getProductsByPage = async (args) => {
   try {
-    const dom = await getDataUrl(`${args.url}?product_list_limit=${totalPerPage}&p=${args.page}`);
+    const dom = await getDataUrl(
+      `${args.url}?product_list_limit=${totalPerPage}&start=${
+        (args.page - 1) * totalPerPage
+      }&sz=${totalPerPage}`
+    );
     const productsInfo = [];
-    const products = [...dom.window.document.querySelectorAll('.product-items > .product-item')];
-    products.forEach(product => {
-      if (product.querySelector('.product-item-name a').textContent !== '') {
-        const cardPrice = product.querySelector('.card-price .price') ? parseInt(replaceAll(product.querySelector('.card-price .price').textContent.replace('$', ''), '.', '')) : 0;
-        const offerPrice = product.querySelector('.internet-price .price-wrapper') ? parseInt(product.querySelector('.internet-price .price-wrapper').dataset.priceAmount) : 0;
-        let normalPrice = product.querySelector('.normal-price .price-wrapper') ? parseInt(product.querySelector('.normal-price .price-wrapper').dataset.priceAmount) : 0;
+    const products = [
+      ...dom.window.document.querySelectorAll(
+        ".product-grid > .product-tile__item"
+      ),
+    ];
+    products.forEach((product) => {
+      if (product.querySelector(".pdp-link a").textContent !== "") {
+        const cardPrice = product.querySelector(
+          ".prices > .js-tlp-price > .price-value"
+        )
+          ? parseInt(
+              replaceAll(
+                product
+                  .querySelector(".prices > .js-tlp-price > .price-value")
+                  .textContent.replace("$", ""),
+                ".",
+                ""
+              )
+            )
+          : 0;
+        const offerPrice = product.querySelector(
+          ".prices > .js-internet-price > .price-value"
+        )
+          ? parseInt(
+              replaceAll(
+                product
+                  .querySelector(".prices > .js-internet-price > .price-value")
+                  .textContent.replace("$", ""),
+                ".",
+                ""
+              )
+            )
+          : 0;
+        let normalPrice = product.querySelector(
+          ".prices > .js-normal-price > .price-value"
+        )
+          ? parseInt(
+              replaceAll(
+                product
+                  .querySelector(".prices > .js-normal-price > .price-value")
+                  .textContent.replace("$", ""),
+                ".",
+                ""
+              )
+            )
+          : 0;
         normalPrice = normalPrice === 0 ? offerPrice : normalPrice;
+
+        const productUrl = product.querySelector(".pdp-link a").href;
         productsInfo.push({
           store: STORE_NAME,
-          sku: product.dataset.id,
-          name: product.querySelector('.product-item-name a').textContent,
-          description: product.querySelector('.product-item-name a').textContent,
-          brand: product.querySelector('.product-item-brand').textContent,
-          url: product.querySelector('.product-item-name a').href,
-          images: [product.querySelector('.product-image-photo').src],
-          thumbnail: product.querySelector('.product-image-photo').src,
+          sku: product.querySelector(".product-tile__wrapper").dataset.pid,
+          name: product.querySelector(".pdp-link a").textContent,
+          description: product.querySelector(".pdp-link a").textContent,
+          brand: replaceAll(
+            product.querySelector(".tile-brand p").textContent,
+            "\n",
+            ""
+          ).trim(),
+          url: productUrl.includes(STORES[storeKey].baseUrl)
+            ? productUrl
+            : `${STORES[storeKey].baseUrl}${productUrl}`,
+          images: [product.querySelector(".tile-image").src],
+          thumbnail: product.querySelector(".tile-image").src,
           category: args.category.url,
           categoryName: args.category.name,
-          discountPercentage: cardPrice !== 0
-            ? (100 - Math.round((cardPrice*100) / normalPrice))
-            : offerPrice !== 0
-            ? (100 - Math.round((offerPrice*100) / normalPrice))
-            : 0,
-          discount: cardPrice !== 0
-            ? (normalPrice - cardPrice)
-            : offerPrice !== 0
-            ? (normalPrice - offerPrice)
-            : 0,
+          discountPercentage:
+            cardPrice !== 0
+              ? 100 - Math.round((cardPrice * 100) / normalPrice)
+              : offerPrice !== 0
+              ? 100 - Math.round((offerPrice * 100) / normalPrice)
+              : 0,
+          discount:
+            cardPrice !== 0
+              ? normalPrice - cardPrice
+              : offerPrice !== 0
+              ? normalPrice - offerPrice
+              : 0,
           normalPrice: normalPrice,
           offerPrice: offerPrice,
           cardPrice: cardPrice,
           isOutOfStock: false,
           isUnavailable: false,
-          version: lastVersion
+          version: lastVersion,
         });
       }
     });
-    
+
     return {
       category: args.category.name,
-      products: productsInfo
+      products: productsInfo,
     };
-  } catch (e){
-    log.error(`[${STORE_NAME}][${args.url}?product_list_limit=${totalPerPage}&p=${args.page}]`, e);
+  } catch (e) {
+    log.error(
+      `${args.url}?product_list_limit=${totalPerPage}&start=${
+        (args.page - 1) * totalPerPage
+      }&sz=${totalPerPage}`,
+      e
+    );
     return {
       category: args.category.name,
       products: [],
     };
   }
-}
+};
 /**
  * Permite obtener el total de páginas de una categoria
  * @param  {string} url - URL de la categoria de la cual se desea obtener el total de páginas
@@ -75,15 +134,20 @@ const getProductsByPage = async (args) => {
  */
 const getTotalPages = async (category) => {
   try {
-    const dom = await getDataUrl(`${category.url}?product_list_limit=${totalPerPage}`);
-    const totalProducts = parseInt(dom.window.document.querySelector('.toolbar-number').textContent);
+    const dom = await getDataUrl(
+      `${category.url}?product_list_limit=${totalPerPage}`
+    );
+    const totalProducts = parseInt(
+      [...dom.window.document.querySelectorAll(".filtering__results-count")][1]
+        .textContent
+    );
     return Math.round(totalProducts / totalPerPage);
   } catch (err) {
     return 1;
   }
-}
+};
 
 module.exports = {
   getProductsByPage,
   getTotalPages,
-}
+};
